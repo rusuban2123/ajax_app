c75558403a32f1c3cbb014bca66f9c4192a50ba3
rusuban2123 <shin12032123@gmail.com>
2020-10-05 12:01:04 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,7 @@
 class PostsController < ApplicationController
-
   def index
-  @posts = Post.all.order(id: "DESC")
+    @posts = Post.all.order(id: "DESC")
   end
-
 
   def create
     Post.create(content: params[:content])
@@ -11,15 +9,14 @@ class PostsController < ApplicationController
   end
 
   def checked
-    post = Post.find
-    if post.checked
-      post.update(checked:false)
-    else post.checked
-      post.update(checked:true)
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
     end
 
     item = Post.find(params[:id])
-    render json: {post: item}
+    render json: { post: item }
+  end
 end
-
-
